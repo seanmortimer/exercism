be470b89db4252affeed31fabc514012beab2dc8
Sean Mortimer <sean.mortimer@gmail.com>
2020-11-08 18:00:48 -0700

    Complete Resistor Duo Exercise JS

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -3,6 +3,22 @@
 // convenience to get you started writing code faster.
 //
 
-export const decodedValue = () => {
-  throw new Error("Remove this statement and implement this function");
+
+const bandColors = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5, 
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+}
+
+export const decodedValue = (colors) => {
+  const value1 = bandColors[colors[0]];
+  const value2 = bandColors[colors[1]];
+  return 10 * value1 + value2;
 };
