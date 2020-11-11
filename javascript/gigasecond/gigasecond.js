2e9c3ecffafac986406a8317402caef59f5f290c
Sean Mortimer <sean.mortimer@gmail.com>
2020-11-10 23:21:11 -0700

    Complete Gigasecond JS exercise

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const gigasecond = () => {
-  throw new Error("Remove this statement and implement this function");
+export const gigasecond = (moment) => {
+  return new Date(Number(moment) + 1E12);
 };
